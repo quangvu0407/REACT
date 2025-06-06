9148f523ab713155bc5dea558877113ac6ba77df
quangvu0407 <trantuyenquang0407@gmail.com>
2025-06-06 21:08:27 +0700

    video 23

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,59 +1,19 @@
 // Class component
 //functional component
 import React from 'react';
-
-
+import UserInfor from './UserInfor';
+import DisplayInfor from './DisplayInfor';
 class MyComponents extends React.Component {
     //JSX
-
-    state = {
-        name: 'quangquang',
-        address: 'Ha Noi',
-        age: 26
-    }
-
-    handleClick(event) {
-        console.log(event)
-        // console.log("random " + Math.floor((Math.random() * 100)) + 1)
-        this.setState({
-            name: 'viet',
-            age: Math.floor((Math.random() * 100) + 1)
-        }
-        )
-    }
-
-    handleMouseOver(event) {
-        // console.log(event.pageX)
-    }
-
-    handleOnChangeInput =(event) =>{
-        this.setState({
-            name: event.target.value
-        })
-        console.log(event.target.value)
-    }
-
-    handleOnSubmit = (event) =>{
-        // event.preventDefault();
-        // console.log(this.state)
-    }
-
     render() {
         return (
             <div>
-                My name is {this.state.name} and I am from {this.state.age}
-                {/* {Math.random()} */}
-                {/* <button onMouseOver={this.handleMouseOver}>handle me</button> */}
-                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
-                <form onSubmit={(event) => {this.handleOnSubmit(event)}}>
-                    <input 
-                    type="text"
-                    onChange={(event) => {this.handleOnChangeInput(event)}} 
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
+                <br/>
+                <DisplayInfor name="quang" age="26"/>
+                <hr/>
+                <DisplayInfor name="eric" age="18"/>
             </div>
-
         );
     }
 }
---
+++ src/components/UserInfor.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class UserInfor extends React.Component{
+    
+    state = {
+        name: 'quangquang',
+        address: 'Ha Noi',
+        age: 26
+    }
+
+    handleClick(event) {
+        console.log(event)
+        // console.log("random " + Math.floor((Math.random() * 100)) + 1)
+        this.setState({
+            name: 'viet',
+            age: Math.floor((Math.random() * 100) + 1)
+        }
+        )
+    }
+
+    handleMouseOver(event) {
+        // console.log(event.pageX)
+    }
+
+    handleOnChangeInput = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnChangeInputAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
+    }
+    render() {
+        return(
+            <div>
+                My name is {this.state.name} and I am from {this.state.age}
+                {/* {Math.random()} */}
+                {/* <button onMouseOver={this.handleMouseOver}>handle me</button> */}
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
+                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <label>your name: </label>
+                    <input
+                        value= {this.state.name}
+                        type="text"
+                        onChange={(event) => { this.handleOnChangeInput(event) }}
+                    />
+                    <button>Submit</button>
+
+                    <label>your age: </label>
+                    <input
+                        value= {this.state.age}
+                        type="text"
+                        onChange={(event) => { this.handleOnChangeInputAge(event) }}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+export default UserInfor;
