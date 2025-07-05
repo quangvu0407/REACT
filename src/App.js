30570e54ef508a7dae4b67f89e838a8aee692627
quangvu0407 <trantuyenquang0407@gmail.com>
2025-07-05 17:21:29 +0700

    video 49

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 // import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from "react-router-dom";
-
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
+      <div className='header-containet'>
+        <Header />
+      </div>
+
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+
+        <div className='app-content'>
+          <Outlet />
+        </div>
+      </div>
+
+      {/* <div>
         test Link
         <div>
           <button>
@@ -18,7 +30,7 @@ const App = () => {
           </button>
 
         </div>
-      </div>
+      </div> */}
     </div >
   );
 }
