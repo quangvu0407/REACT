cf532f75b30149e7f3e813a8fffdbe7fdf0ddf31
quangvu0407 <trantuyenquang0407@gmail.com>
2025-09-08 13:18:44 +0700

    video 69

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+import './Login.scss';
 const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleClick = () => {
+        alert("login");
+    }
+
     return (
-        <div>
-            Login
+        <div className="login-container">
+            <div className="header">
+                Don't have an account yet?
+            </div>
+            <div className="title col-4 mx-auto">
+                QuangQuang
+            </div>
+            <div className="welcome col-4 mx-auto">
+                Hello, Who's this?
+            </div>
+            <div className="content-form col-4 mx-auto">
+                <div className="form-group">
+                    <label>Email</label>
+                    <input
+                        type={"email"}
+                        className="form-control"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    ></input>
+                </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type={"password"}
+                        className="form-control"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    ></input>
+                </div>
+                <span className='forgot-password'>Forgot a password</span>
+                <div>
+                    <button 
+                    className='btn-submit'
+                    onClick={() => handleClick()}
+                    >Login</button>
+                </div>
+            </div>
         </div>
     )
 }
