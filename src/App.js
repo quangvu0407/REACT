a07b2fbc864b092c67f6684f52e061e3f9344cfc
quangvu0407 <trantuyenquang0407@gmail.com>
2025-09-13 15:33:17 +0700

    vv

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponents from './components/MyComponents';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
