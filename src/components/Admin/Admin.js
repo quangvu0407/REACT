802c00356284394164e8be3af8277239128c84e8
quangvu0407 <trantuyenquang0407@gmail.com>
2025-09-28 15:45:30 +0700

    video 110

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,11 +3,14 @@ import './Admin.scss';
 import { FaHeart, FaBars } from 'react-icons/fa';
 import { useState } from "react";
 import { Outlet } from 'react-router-dom';
-import PerfectScrollbar from 'react-perfect-scrollbar'
-
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useTranslation } from 'react-i18next';
 import 'react-toastify/dist/ReactToastify.css';
+import Language from "../Header/Language";
 const Admin = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
     return (
         <div className="admin-container">
             <div className="admin-sidebar">
@@ -16,7 +19,17 @@ const Admin = () => {
 
             <div className="admin-content">
                 <div className="admin-header">
-                    <FaBars onClick={() => setCollapsed(!collapsed)} />
+                    <span onClick={() => setCollapsed(!collapsed)}>
+                        <FaBars className="leftside" />
+                    </span>
+                    <div className="rightside">
+                        <Language />
+                        <NavDropdown title={t("header.setting")} id="basic-nav-dropdown" className="custom-dropdown">
+                            <NavDropdown.Item >{t("header.profile")}</NavDropdown.Item>
+                            <NavDropdown.Item >{t("header.logout")}</NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
+
                     {/* <FaBars onClick={() => setCollapsed(!toggled)} /> */}
                 </div>
 
